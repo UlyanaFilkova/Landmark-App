c809d564c24a8e78d3c03025f35cbcf586cdf091
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-12-21 14:10:12 +0300

    add rating while adding new place

--- src/services/place.ts
+++ src/services/place.ts
@@ -5,11 +5,18 @@ const placesCollection = collection(firestore, 'places')
 const ratingsCollection = collection(firestore, 'ratings')
 
 export const addPlace = async (place: Omit<Place, 'id'>): Promise<{ id: string }> => {
-    try {
-      const docRef = await addDoc(placesCollection, place)
-      return { id: docRef.id }
-    } catch (error) {
-      console.error('Error adding place:', error)
-      throw new Error('Error adding place to Firestore')
+  try {
+    const placeDocRef = await addDoc(placesCollection, place)
+    const rating: Omit<Rating, 'id'> = {
+      rating: place.rating,
+      userId: place.authorId,
+      placeId: placeDocRef.id,
     }
+    const ratingDocRef = await addDoc(ratingsCollection, rating)
+    console.log(ratingDocRef.id)
+    return { id: placeDocRef.id }
+  } catch (error) {
+    console.error('Error adding place:', error)
+    throw new Error('Error adding place to Firestore')
   }
+}
