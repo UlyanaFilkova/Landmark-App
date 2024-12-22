592194749289e53be14719bc9bb27fee74c70e7d
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-12-22 21:27:56 +0300

    add place links

--- src/components/map/TopPlaces.vue
+++ src/components/map/TopPlaces.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="top-places_container">
     <h1 class="top-places_h1">Top Places</h1>
-    <div v-for="(place, index) in displayedPlaces" :key="place.id" class="place-card-container">
+    <div v-for="(place, index) in displayedPlaces" :key="place.id" class="place-card-container" click>
       <span class="place-number">{{ index + 1 }}</span>
       <PlaceCard :place="place" />
     </div>
