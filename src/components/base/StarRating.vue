63dcf1eca8173ae837f88bca2061e52b1c5daceb
Ulyana Filkova <ulyanafilkova56@gmail.com>
2024-12-26 13:11:18 +0300

    customize style ratings

--- src/components/base/StarRating.vue
+++ src/components/base/StarRating.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="text-center">
-    <Rating v-model:rating="rating" readOnly />
-
-    <pre>{{ rating }}</pre>
-  </div>
+  <Rating
+    v-model:rating="rating"
+    :readOnly="readonly"
+    :increment="increment"
+    :fixedPoints="1"
+    :activeColor="activeColor"
+    :inactiveColor="inactiveColor"
+    :starSize="starSize"
+    :text-class="textClass"
+    showRating
+    :class="{ 'pointer-cursor': !readonly }"
+  />
 </template>
 
 <script setup lang="ts">
@@ -19,13 +26,54 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
+  increment: {
+    type: Number,
+    default: 0.1,
+  },
+  starSize: {
+    type: Number,
+    default: 24,
+  },
+  textClass: {
+    type: String,
+    default: 'normal',
+  },
 })
 
 const emit = defineEmits(['update:rating'])
 
 const rating = ref(props.rating)
+const activeColor = '#ffc013'
+const inactiveColor = '#aaa'
 
 watch(rating, (newValue) => {
   emit('update:rating', newValue)
 })
 </script>
+
+<style>
+.sr-only span {
+  display: none;
+}
+
+.v-star-rating-stars {
+  display: flex;
+  align-items: center;
+}
+
+.normal {
+  margin-left: 5px;
+  font-size: 16px;
+  color: #555;
+}
+
+.big {
+  margin-left: 5px;
+  font-size: 20px;
+  color: #555;
+}
+
+.pointer-cursor {
+  cursor: pointer;
+}
+</style>
